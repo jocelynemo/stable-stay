740a7a968bb7cab6223a2b7001cb775f10d69177
jocelynemo <jocelyn.e.mo@gmail.com>
2026-05-10 15:17:53 -0400

    set up session, potential helpers, bycrypt, minor cleanup

--- app.js
+++ app.js
@@ -2,15 +2,31 @@ import express from 'express';
 import exphbs from 'express-handlebars';
 import session from 'express-session';
 import configRoutes from './routes/index.js';
+import { helpers } from './helpers.js';
 
 const app = express();
 
-app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main', helpers}));
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+
+app.use(session({
+  name: 'StableStaySession',
+  secret: 'stablestay_secret_key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 8 }
+}));
+
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  res.locals.isLoggedIn = !!req.session.user;
+  res.locals.isAdmin = req.session.user?.isAdmin || false;
+  next();
+});
 
 configRoutes(app);
 
